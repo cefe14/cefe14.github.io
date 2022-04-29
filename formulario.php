782c2104840f8dfdaaba2b806216f3d271bbb736
cefe14 <104643645+cefe14@users.noreply.github.com>
2022-04-29 13:50:47 -0300

    Update formulario.php

--- formulario.php
+++ formulario.php
@@ -13,7 +13,29 @@
 
 <body>
 
-<?php include ('barra.php') ?> >
+       <!-- Barra de Menu -->
+   <header class="header">
+        <nav class="barra">
+            <a href="#" class="logo barra-enlace">ProyectosDB</a>
+            <button class="barra-opciones" aria-label="Abrir Menu">
+                <i class="fas fa-bars"></i>
+            </button>
+            <ul class="barra-menu">
+      
+                <li class="barra-menu-item"><a href="home.php" class="barra-menu-enlace barra-enlace">Home</a></li>
+                <li class="barra-menu-item"><a href="#" class="barra-menu-enlace barra-enlace">Sistema Operativo</a>
+                <ul class="barra-menu-desplegable">
+                    <li class="barra-menu-item-desplegable"><a href="windows.php" class="barra-menu-enlace-desplegable">Windows</a></li>
+                    <li class="barra-menu-item-desplegable"><a href="linux.php" class="barra-menu-enlace-desplegable">Linux</a></li>
+                    <li class="barra-menu-item-desplegable"><a href="mac.php" class="barra-menu-enlace-desplegable">Mac Os</a></li>
+                </ul>
+                </li>
+                <li class="barra-menu-item"><a href="#" class="barra-menu-enlace barra-enlace">Streaming</a></li>
+                <li class="barra-menu-item"><a href="#" class="barra-menu-enlace barra-enlace">Donación</a></li>
+                <li class="barra-menu-item"><a href="formulario.php" class="barra-menu-enlace barra-enlace">Suscribete</a></li>
+            </ul>
+        </nav>
+   </header>
 
             <form class="Formulario_HD" method="post">
                   <h2>Suscribete a mi pagina</h2>
@@ -62,6 +84,33 @@
 </form>
 
 <?php include ('registar.php'); ?>
-<?php include ('piedepagina.php'); ?>
+<!-- Pie de Pagina -->
+    <footer class="pie-pagina">
+        <div class="seccion1">
+            <div class="caja">
+                <h2>¿Que es Anime Sin relleno?</h2>
+                <p>Anime <b>sin relleno</b> es apegarse a la historia original del mangaka, en otras palabras,
+                realizar un recorte a la animación, tal que, se parezca al manga y armando así capitulos toalmente
+                nuevos.</p>
+            </div>
+            <div class="caja">
+                <h2>Sobre Nosotros</h2>
+                <p>Somos varios editores realizando proyectos de manera indivual, actualmente conformamos 3 personas, para diferentes proyectos.</p>
+                <p>Actualmente nos centramos en Anime sin relleno, como Ranma 1/2, Dragón Ball Kai, One Piece, Black Clover, entre otros.</p>
+            </div>
+            <div class="caja">
+               <h2>siguenos</h2>
+               <div class="red-social">
+                <a href="https://www.facebook.com/proyectosDB" Target="_blank" class="fa fa-facebook" ></a>
+                <a href="https://www.instagram.com/ceferinoa14/" Target="_blank" class="fa fa-instagram"></a>
+                <a href="https://discord.com/invite/5bxu3EeQhU" Target="_blank"class="fab fa-discord" ></a>
+                <a href="https://www.youtube.com/channel/UCDdvIbJdpatB1qcEJ-uQEvA" Target="_blank"class="fa fa-youtube"></a>
+               </div>
+            </div>
+        </div>
+        <div class="seccion2">
+            <small>&copy; 2016-2022 <b>ProyectosDB</b> - Todos los derechos reservados </small>
+        </div>
+    </footer>
 
 </body>
